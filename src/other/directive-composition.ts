413fd115fbb3afe8c99f6f2dd2e15169c42201f7
Saeed Bayat <01shadowalker1@gmail.com>
2023-04-20 15:21:35 +0330

    add directive-composition example

---
+++ src/other/directive-composition.ts
@@ -0,0 +1,25 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: "pin",
+//   standalone: true,
+//   template: `<img [src]="'assets/' + image + '.svg'" />`,
+//   hostDirectives: [
+//     { directive: TooltipDirective },
+//     { directive: DragableDirective },
+//     { directive: RotateDirective },
+//   ],
+// })
+// export class PinComponent {}
+
+//////////
+
+// hostDirectives: [
+//   { directive: TooltipDirective, inputs: ["tooltip"] },
+//   {
+//     directive: DragableDirective,
+//     inputs: ["dragzone"],
+//     outputs: ["pinGrabbed"],
+//   },
+//   { directive: RotateDirective, inputs: ["rotate"] },
+// ];
